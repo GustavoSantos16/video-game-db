fe51e8c8877c0609136ef60489a2a9bef0187e00
Gustavo dos Santos <gustavogugueira@gmail.com>
2022-07-19 15:28:05 -0300

    feat: Game details

--- ng-video-game-db/src/app/components/home/home.component.ts
+++ ng-video-game-db/src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { APIResponse, Game } from 'src/app/models';
 import { HttpService } from 'src/app/services/http.service';
 
@@ -8,17 +9,20 @@ import { HttpService } from 'src/app/services/http.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   public sort!: string;
   public games: Array<Game> | undefined;
+  private routeSub: Subscription | undefined;
+  private gameSub: Subscription | undefined;
 
   constructor(
     private httpService: HttpService,
+    private router: Router,
     private activatedRoute: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe((params: Params) => {
+   this.routeSub = this.activatedRoute.params.subscribe((params: Params) => {
       if(params['game-search']){
         this.searchGames('metacrit', params['game-search']);
       }else{
@@ -29,10 +33,24 @@ export class HomeComponent implements OnInit {
 
 
   searchGames(sort: string, search?: string){
-    this.httpService.getGameList(sort, search).subscribe((gamelist: APIResponse<Game>) => {
+    this.gameSub = this.httpService.getGameList(sort, search).subscribe((gamelist: APIResponse<Game>) => {
       this.games = gamelist.results;
       console.log(this.games);
     });
+  }
+
+  openGameDetails(id: string):void {
+    this.router.navigate(['details', id]);
+  }
+
+  ngOnDestroy():void {
+    if(this.gameSub){
+      this.gameSub.unsubscribe();
+    }
+
+    if(this.routeSub) {
+      this.routeSub.unsubscribe;
+    }
   }
 
 }
